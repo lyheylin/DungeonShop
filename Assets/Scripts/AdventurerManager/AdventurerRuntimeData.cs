1fdf2794b4175ce8fe2e55e3bd9bf5337e575a3a
lyheylin <lyheylin@gmail.com>
2025-05-26 20:34:07 +0800

    Added basic implementation of adventurer runtime data initialization.

--- Assets/Scripts/AdventurerManager/AdventurerRuntimeData.cs
+++ Assets/Scripts/AdventurerManager/AdventurerRuntimeData.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 public class AdventurerRuntimeData {
     public AdventurerDataSO AdventurerBase;
@@ -11,18 +12,39 @@ public class AdventurerRuntimeData {
     public int Defense;
     public int Alertness;
     public int LightLevel;
-    public List<AdventurerTrait> ActiveTraits;
+    public List<AdventurerTraitSO> ActiveTraits;
     public List<AdventurerInventoryItem> ActiveItems;
 
     public AdventurerRuntimeData(AdventurerDataSO baseData) {
         AdventurerBase = baseData;
         // Initialize stats based on base + equipment + skills
+
+        MaxHP = Mathf.RoundToInt(baseData.GetConstitution() * 1.5f);
+        CurrentHP = MaxHP;
+        Attack = baseData.GetStrength();
+        Defense = Mathf.RoundToInt(baseData.GetConstitution() * 0.2f + baseData.GetAgility() * 0.8f);
+        Alertness = 0;
+        LightLevel = 0;
+
+        ActiveTraits = baseData.GetTraits();
+
+        ApplyTraits();
     }
 
     public void ApplyConsumable(ItemDataSO item) {
         // Use logic based on item effect
     }
 
+    private void ApplyTraits() {
+        foreach (var trait in ActiveTraits) {
+            trait.ApplyAllEffects(this);
+        }
+    }
+
+
+
+
+    // Specific implementations
     private bool canUseMultipleItems = false;
     public bool CanUseMultipleItems() => canUseMultipleItems;
     internal void SetCanUseMultipleItems(bool value) {
